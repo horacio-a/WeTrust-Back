cc4bd7223e51093dc52c3d29775e605c92b960d3
horacio albornoz <horaciomatiasalbornoz@gmail.com>
2022-12-04 18:13:48 -0300

    img url add

--- routes/api.js
+++ routes/api.js
@@ -9,12 +9,15 @@ router.get('/productos', async function (req, res, next) {
     let clothing_size = await productosModel.GetClothingSize()
     let shoe_size = await productosModel.GetShoeSize()
     for (let i = 0; i < producto.length; i++) {
-        console.log(producto[i].name)
+
         if (producto[i].category == 'clothing') {
             for (let index = 0; index < clothing_size.length; index++) {
                 if (clothing_size[index].name == producto[i].name) {
+                    const img = cloudinary.url(producto[index].img,) 
+                    console.log(img)
                     data.push({
                         produto: producto[i],
+                        img,
                         talles: clothing_size[index]
                     })
 
@@ -25,8 +28,10 @@ router.get('/productos', async function (req, res, next) {
         if (producto[i].category == 'shoe') {
             for (let index = 0; index < shoe_size.length; index++) {
                 if (shoe_size[index].name == producto[i].name) {
+                    const img = cloudinary.url(producto[index].img,) 
                     data.push({
                         produto: producto[i],
+                        img,
                         talles: shoe_size[index]
                     })
 
@@ -35,104 +40,12 @@ router.get('/productos', async function (req, res, next) {
             }
         }
     }
-    console.log(data)
     return res.json(data)
 
 
 });
 
-router.get('/recursos/id/:id', async function (req, res, next) {
-    var id = req.params.id;
-    var recursos = await recursosModel.getRecursosById(id)
-    if (typeof recursos !== 'undefined') {
-        if (recursos.img_muestra) {
-            recursos.img_muestra = cloudinary.url(recursos.img_muestra, {
-                crop: 'crop'
-            });
-        } else {
-            recursos.img_muestra = ''
-        }
 
-        if (recursos.img_codejs) {
-            recursos.img_codejs = cloudinary.url(recursos.img_codejs, {
-                crop: 'crop'
-            });
-        } else {
-            recursos.img_codejs = 'https://res.cloudinary.com/dioe9ktns/image/upload/v1664126222/DefaultJS_d8oahk.png'
-        }
-
-        if (recursos.img_codecss) {
-            recursos.img_codecss = cloudinary.url(recursos.img_codecss, {
-                crop: 'crop'
-            });
-        } else {
-            recursos.img_codecss = 'https://res.cloudinary.com/dioe9ktns/image/upload/v1664126222/DefaultCSS_eh7fxh.png'
-        }
-
-
-        if (recursos.img_codehtml) {
-            recursos.img_codehtml = cloudinary.url(recursos.img_codehtml, {
-                crop: 'crop'
-            });
-        } else {
-            recursos.img_codehtml = 'https://res.cloudinary.com/dioe9ktns/image/upload/v1664126222/DefaultHMTL_prj7ym.png'
-        }
-
-
-        return res.json(recursos)
-    }
-    return res.json({
-        mensaje: 'id no encontrado'
-    })
-
-
-});
-
-
-router.get('/recursos/aleatorio', async function (req, res, next) {
-
-    var recursos = await recursosModel.getRecursosRand()
-    if (typeof recursos !== 'undefined') {
-        if (recursos.img_muestra) {
-            recursos.img_muestra = cloudinary.url(recursos.img_muestra, {
-                crop: 'crop'
-            });
-        } else {
-            recursos.img_muestra = ''
-        }
-
-        if (recursos.img_codejs) {
-            recursos.img_codejs = cloudinary.url(recursos.img_codejs, {
-                crop: 'crop'
-            });
-        } else {
-            recursos.img_codejs = 'https://res.cloudinary.com/dioe9ktns/image/upload/v1664126222/DefaultJS_d8oahk.png'
-        }
-
-        if (recursos.img_codecss) {
-            recursos.img_codecss = cloudinary.url(recursos.img_codecss, {
-                crop: 'crop'
-            });
-        } else {
-            recursos.img_codecss = 'https://res.cloudinary.com/dioe9ktns/image/upload/v1664126222/DefaultCSS_eh7fxh.png'
-        }
-
-
-        if (recursos.img_codehtml) {
-            recursos.img_codehtml = cloudinary.url(recursos.img_codehtml, {
-                crop: 'crop'
-            });
-        } else {
-            recursos.img_codehtml = 'https://res.cloudinary.com/dioe9ktns/image/upload/v1664126222/DefaultHMTL_prj7ym.png'
-        }
-
-
-        return res.json(recursos)
-    }
-
-
-
-});
 
 
 
